71c505b8530a6a7f1b083f6759cb1abf25fd7050
Evan Sun <553274440@qq.com>
2021-11-29 11:22:49 +0800

    改成发布到 docs 分支

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -13,7 +13,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/evansun1995/evansun1995.github.io.git  master:dist
+git push -f https://github.com/evansun1995/evansun1995.github.io.git  master:docs
 
 # cd -
 # 执行bash命令防止窗口自动关闭
